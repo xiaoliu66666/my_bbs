c5bb4be31e717b8c3136c6e8b0a5a7dd4e207b06
xiaoliu66666 <c650156760@163.com>
2019-01-25 18:15:20 +0800

    添加权限

--- apps/cms/models.py
+++ apps/cms/models.py
@@ -3,6 +3,45 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
+class CMSPersmission(object):
+    # 255的二进制方式表示11111111
+    ALL_PERMISSION = 0b11111111
+    # 访问者权限
+    VISITOR = 0b00000001
+    # 管理帖子权限
+    POSTER = 0b00000010
+    # 管理评论的权限
+    COMMENTER = 0b00000100
+    # 管理板块的权限
+    BOARDER = 0b00001000
+    # 管理前台用户的权限
+    FRONTUSER = 0b00010000
+    # 管理后台用户的权限
+    CMSUSER = 0b00100000
+    # 管理后台管理员的权限
+    ADMIN = 0b01000000
+
+
+cms_role_user = db.Table(
+    'cms_role_user',
+    db.Column('cms_role_id', db.Integer, db.ForeignKey('cms_role.id'), primary_key=True),
+    db.Column('cms_user_id', db.Integer, db.ForeignKey('cms_user.id'), primary_key=True)
+)
+
+
+class CMSRole(db.Model):
+    __tablename__ = "cms_role"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    desc = db.Column(db.String(200), nullable=True)
+    create_time = db.Column(db.DateTime, default=datetime.now)
+    permissions = db.Column(db.Integer, default=CMSPersmission.VISITOR)
+    # 这里secondary参数是用于多对多关系, 它的值就是关联表的名字,
+    # backref参数是给user增加一个roles的属性，使用列表存储
+    # 以后就可以用user.roles来获取 CMSRole id属性 的列表
+    users = db.relationship("CMSUser", secondary=cms_role_user, backref='roles')
+
+
 class CMSUser(db.Model):
     __tablename__ = "cms_user"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -15,7 +54,7 @@ class CMSUser(db.Model):
         self.username = username
         self.password = password
         self.email = email
-    
+
     @property
     def password(self):
         return self._password
@@ -27,3 +66,25 @@ class CMSUser(db.Model):
     def check_password(self, raw_pwd):
         result = check_password_hash(self.password, raw_pwd)
         return result
+
+    @property
+    def permissions(self):
+        if not self.roles:
+            return 0
+
+        all_permissions = 0
+        for role in self.roles:
+            permission = role.permissions
+            all_permissions |= permission
+        return all_permissions
+
+    def has_permission(self, permission):
+        # 将传进去的权限跟用户的所有权限进行 与 运算,
+        # 如果结果依旧等于传进去的权限, 则说明拥有这个权限
+        return permission & self.permissions == permission
+
+    @property
+    def is_developer(self):
+        return self.has_permission(CMSPersmission.ALL_PERMISSION)
+
+
--- manage.py
+++ manage.py
@@ -5,6 +5,8 @@ from bbs import create_app
 from apps.cms import models as cms_models
 
 CMSUser = cms_models.CMSUser
+CMSRole = cms_models.CMSRole
+CMSPermission = cms_models.CMSPersmission
 
 app = create_app()
 manager = Manager(app)
@@ -23,6 +25,60 @@ def create_cms_user(username, password, email):
     print("添加成功")
 
 
+@manager.command
+def create_role():
+    # 访问者(可以修改个人信息)
+    visitor = CMSRole(name='访问者', desc='可以修改个人信息')
+
+    # 版主角色(修改个人信息，管理帖子，管理评论，管理前台用户)
+    operator = CMSRole(name='版主', desc='管理帖子，评论，前台用户')
+    operator.permissions = (CMSPermission.VISITOR |
+                            CMSPermission.POSTER |
+                            CMSPermission.COMMENTER |
+                            CMSPermission.FRONTUSER)
+    # 管理员(拥有绝大部分权限)
+    admin = CMSRole(name='管理员', desc='拥有本系统所有权限')
+    admin.permissions = (CMSPermission.VISITOR |
+                         CMSPermission.POSTER |
+                         CMSPermission.COMMENTER |
+                         CMSPermission.BOARDER |
+                         CMSPermission.FRONTUSER |
+                         CMSPermission.CMSUSER)
+    # 开发者
+    developer = CMSRole(name='开发者', desc='开发人员专用')
+    developer.permissions = CMSPermission.ALL_PERMISSION
+
+    db.session.add_all([visitor, operator, admin, developer])
+    db.session.commit()
+
+
+@manager.option('-e', '--email', dest='email')
+@manager.option('-n', '--name', dest='name')
+def add_user_to_role(email, name):
+    user = CMSUser.query.filter_by(email=email).first()
+    if user is not None:
+        role = CMSRole.query.filter_by(name=name).first()
+        if role is not None:
+            role.users.append(user)
+            db.session.commit()
+            print('用户{}添加到角色{}成功'.format(email, name))
+        else:
+            print('没有这个角色：{}'.format(name))
+    else:
+        print('没有这个用户：{}'.format(email))
+
+
+# @manager.command
+# def test_permission():
+#     user = CMSUser.query.first()
+#     role = CMSRole.query.first()
+#     if user.is_developer:
+#         print('用户{}有开发者的权限'.format(user.email))
+#         print("用户的角色:{}".format(user.roles))
+#         print("角色对应的用户:{}".format(role.users))
+#     else:
+#         print('用户{}没有开发权限'.format(user.email))
+
+
 if __name__ == '__main__':
     manager.run()
-
