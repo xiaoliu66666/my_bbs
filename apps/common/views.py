2f4d843aa2b4f9f4a788ede6ab6918de37b0d6f1
xiaoliu66666 <c650156760@163.com>
2019-01-27 22:51:43 +0800

    Revert "memcache存储短信验证码"
    This reverts commit da6e41e9670807d19015bf618bc847323883af19.

--- apps/common/views.py
+++ apps/common/views.py
@@ -1,9 +1,5 @@
-from io import BytesIO
+from flask import Blueprint
 
-from flask import Blueprint, make_response
-
-from utils.captcha import Captcha
-from utils import cache
 
 main = Blueprint("common", __name__, url_prefix="/common")
 
@@ -11,16 +7,3 @@ main = Blueprint("common", __name__, url_prefix="/common")
 @main.route("/")
 def index():
     return "common index"
-
-
-@main.route("/captcha/")
-def graph_capcha():
-    text, image = Captcha.gene_graph_captcha()
-    # 将图形验证码按照 文本：文本的格式存储到memcache中
-    cache.set(text.lower(), text.lower())
-    out = BytesIO()
-    image.save(out, "png")
-    out.seek(0)
-    resp = make_response(out.read())
-    resp.content_type = 'image/png'
-    return resp
