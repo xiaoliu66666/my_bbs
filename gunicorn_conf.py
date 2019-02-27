50de2b3b6302b096727dafa70f64bef623552b45
xiaoliu66666 <c650156760@163.com>
2019-02-27 10:29:24 +0800

    部署相关配置

---
+++ gunicorn_conf.py
@@ -0,0 +1,2 @@
+bind = '0.0.0.0:8000'
+pid = '/tmp/my_bbs.pid'
---
+++ wsgi.py
@@ -0,0 +1,28 @@
+import sys
+from os.path import abspath
+from os.path import dirname
+from bbs import app
+
+
+sys.path.insert(0, abspath(dirname(__file__)))
+application = app.app
+
+"""
+建立一个软连接（右边指向左边）
+ln -s /var/www/my_bbs/my_bbs.conf /etc/supervisor/conf.d/my_bbs.conf
+
+➜  ~ cat /etc/supervisor/conf.d/my_bbs.conf
+
+[program:bbs]
+command=/usr/local/bin/gunicorn wsgi -c gunicorn_conf.py
+directory=/var/www/my_bbs
+autostart=true
+autorestart=true
+
+
+
+
+/usr/local/bin/gunicorn wsgi
+--bind 0.0.0.0:8000
+--pid /tmp/blog.pid
+"""
